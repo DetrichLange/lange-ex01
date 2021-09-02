9d1a1762fe8ae0293b9e3d62e59df439e254d16e
DetrichLange <detrich.lange@gmail.com>
2021-09-02 18:06:55 -0400

    Initial commit

---
+++ src/main/java/example/Solution01.java
@@ -0,0 +1,26 @@
+package example;
+import java.util.Scanner;
+
+/*
+Create a program that prompts for your name and prints a greeting using your name.
+    Perform the input, string concatenation, and output in separate statements.
+
+1: Prompt user for name
+2: Take line input from user and set to variable
+3: Concatenate output statement using inputted name
+4: Print output statement
+ */
+
+public class Solution01
+{
+    private static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args)
+    {
+        System.out.print("What is your name? ");
+        String Inputname = input.nextLine();
+
+        String Finalstring = "Hello, " + Inputname + ", nice to meet you!";
+        System.out.println(Finalstring);
+    }
+}
