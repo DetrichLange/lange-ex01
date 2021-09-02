37d4fe92286da2d384bf8c822b900a9033340d4b
DetrichLange <detrich.lange@gmail.com>
2021-09-02 18:13:23 -0400

    Added initial comment

--- src/main/java/example/Solution01.java
+++ src/main/java/example/Solution01.java
@@ -1,15 +1,18 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 1 Solution
+ *  Copyright 2021 Detrich Lange
+ */
+
 package example;
 import java.util.Scanner;
 
-/*
-Create a program that prompts for your name and prints a greeting using your name.
+/*  Create a program that prompts for your name and prints a greeting using your name.
     Perform the input, string concatenation, and output in separate statements.
 
 1: Prompt user for name
 2: Take line input from user and set to variable
 3: Concatenate output statement using inputted name
-4: Print output statement
- */
+4: Print output statement   */
 
 public class Solution01
 {
